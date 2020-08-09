2eb14c30ffb35dc87d7bb028e7d30bf674823146
Russell Sutter <russell.sutter@gmail.com>
2020-08-09 22:45:14 +0000

    Automatically backed up by Learn

--- lib/class_methods_module.rb
+++ lib/class_methods_module.rb
@@ -1,6 +1,6 @@
 module MetaDancing
  
   def metadata
-    "This class produces objects that love to dance."
+    "This class produces objects that love to dance and I can change it to whatever I want"
   end
 end
